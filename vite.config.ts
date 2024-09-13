ef8690701012263acdfeaf4b4abdc14e9c10b4e1
ldhldh07 <dhlim07@naver.com>
2024-09-13 17:53:49 +0900

    third commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/harry_quiz/', // 이 부분을 프로젝트에 맞게 설정
   build: {
     outDir: 'build', // 빌드 폴더 이름이 'build'로 설정되어 있는지 확인
   },
